d9b1d3b80676edbed71022a4e455912debfdeed6
cabotwang <sichen_wang@126.com>
2022-07-27 17:09:06 +0800

    update

---
+++ pages/📁_结构化编码.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.subheader("结构化编码")
+st.markdown('''---''')
+# c1, c2, c3, c4, c5, c6, ce, c7 = st.columns([1, 1, 0.2, 0.4, 0.2, 0.4, 2.5, 0.2])
+# system = c1.selectbox('身体系统', ('器官', '骨骼', '血液', '神经'))
+# info = c2.selectbox('描述', ())
+# male = c3.button('男')
+# female = c4.button('女')
+#
+# left = c5.button('左')
+# right = c6.button('右')
+# back = c7.button('↩')
+
+st.text_input('输入想查询的')
